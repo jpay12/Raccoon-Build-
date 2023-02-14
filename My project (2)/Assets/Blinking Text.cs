8ad246849f9f4a631ded64ee1eb31c37d9add838
jpay12 <97638950+jpay12@users.noreply.github.com>
2023-02-14 15:15:13 -0500

    added a script that makes the text blink

---
+++ My project (2)/Assets/Blinking Text.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class BlinkingText : MonoBehaviour
+{
+    public float minTime = 0.05f;
+    public float maxTime = 1.2f;
+
+    private float timer;
+    private Text textFlicker;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
